71bbb51da144572351305098c9fb29008acc5c54
Andres Cardenas <andrescr2503@gmail.com>
2018-10-16 08:08:45 -0500

    Se creó el proyecto MisDispositivos y se agregó cuatro botones a la página principal, junto sus correspondientes páginas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BocinaPage } from '../bocina/bocina';
+import { SmartphonePage } from '../smartphone/smartphone';
+import { LamparaPage } from '../lampara/lampara';
+import { TvPage } from '../tv/tv';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
 export class HomePage {
+
+  bocina = BocinaPage;
+  phone = SmartphonePage;
+  lamp = LamparaPage;
+  tv = TvPage;
 
   constructor(public navCtrl: NavController) {
 
   }
+
+  tele(){
+    this.navCtrl.push(this.tv);
+  }
+
+  boci(){
+    this.navCtrl.push(this.bocina);
+  }
+
+  lampa(){
+    this.navCtrl.push(this.lamp);
+  }
+
+  sphone(){
+    this.navCtrl.push(this.phone);
+  }
+  
 
 }
